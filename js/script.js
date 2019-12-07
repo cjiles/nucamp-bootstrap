de94f94565bae88a1d5b7df36a68ded43d23e006
cjiles <57699863+cjiles@users.noreply.github.com>
2019-12-07 12:19:53 -0600

    Week 4 Assignment

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(function() {
+    //interval of carousel is 2 secs
     $(".carousel").carousel( {interval: 2000 });
+    //on click toggle between pause and play.  When pause is pushed, pause carousel, and show play button.  Vice versa
     $("#carouselButton").click(function(){
         if ($("#carouselButton").children("i").hasClass("fa-pause")) {
             $(".carousel").carousel("pause");
@@ -11,4 +13,14 @@ $(function() {
             $("#carouselButton").children("i").addClass("fa-pause");
         }
     });
+    //on button click show modal
+    $("#reserveButton").click(function() {
+        $("#reserveModal").modal("show");
+    });
+
+    $("#loginButton").click(function() {
+        $("#loginModal").modal("show");
+    });
 });
+
+
